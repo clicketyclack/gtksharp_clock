9da0907b4b11ecfd7be3a4d04043a8756fc39aac
Erik Mossberg <erik.mossberg@gmail.com>
2017-04-23 12:55:35 +0200

    Add TimeHMS class for time calculations.

--- gtksharp_clock/Program.cs
+++ gtksharp_clock/Program.cs
@@ -19,6 +19,162 @@ namespace gtksharp_clock
 		}
 	}
 
+	/// <summary>
+	/// A representation of time, with only hours, minutes and seconds.
+	/// This is intended as a slightly "smart" struct. The class is a wrapper for
+	/// three doubles which can be set via batch update.
+	/// 
+	/// Note that each attribute contains the time data of the more granular attibutes, so
+	/// the time 00:30 will return both CurrentHours as 0.5 and CurrentMinutes as 30.0.
+	/// </summary>
+	public class TimeHMS
+	{
+		private double current_hours;
+		private double current_minutes;
+		private double current_seconds;
+
+		public TimeHMS()
+		{
+			UpdateTime();
+		}
+
+		/// <summary>
+		/// Updates the HMS representation to the current time.
+		/// </summary>
+		public void UpdateTime()
+		{
+			double milliseconds_of_day = DateTime.Now.TimeOfDay.TotalMilliseconds;
+			UpdateTime(milliseconds_of_day);
+		}
+
+		/// <summary>
+		/// Updates the HMS representation to a specified time.
+		/// </summary>
+		/// <param name="milliseconds_of_day">Milliseconds of day.</param>
+		public void UpdateTime(double milliseconds_of_day)
+		{
+			current_hours = (milliseconds_of_day / (60.0 * 60.0 * 1000.0)) % 24.0;
+			current_minutes = (milliseconds_of_day / (60.0 * 1000.0)) % 60.0;
+			current_seconds = (milliseconds_of_day / 1000.0) % 60.0;
+		}
+
+		/// <summary>
+		/// Gets the current hours.
+		/// </summary>
+		/// <value>The current hours.</value>
+		public double CurrentHours
+		{
+			get { return current_hours; }
+		}
+
+		/// <summary>
+		/// Gets the current minutes.
+		/// </summary>
+		/// <value>The current minutes.</value>
+		public double CurrentMinutes
+		{
+			get { return current_minutes; }
+		}
+
+		/// <summary>
+		/// Gets the current seconds.
+		/// </summary>
+		/// <value>The current seconds.</value>
+		public double CurrentSeconds
+		{
+			get { return current_seconds; }
+		}
+
+		/// <summary>
+		/// Convert back to milliseconds of day.
+		/// </summary>
+		/// <value>The milliseconds of day.</value>
+		public double MillisecondsOfDay
+		{
+			get { return current_hours * 3600.0 * 1000.0; }
+		}
+	}
+
+	/// <summary>
+	/// Test harness for TimeHMS.
+	/// </summary>
+	public class TimeHMSTests
+	{
+
+		/// <summary>
+		/// Verify the HMS conversions.
+		/// </summary>
+		/// <param name="milliseconds_of_day">Milliseconds of day.</param>
+		/// <param name="expected_hours">Expected hours.</param>
+		/// <param name="expected_minutes">Expected minutes.</param>
+		/// <param name="expected_seconds">Expected seconds.</param>
+		/// <param name="epsilon">Epsilon.</param>
+		[Test]
+		[TestCase(0, 0, 0, 0, 0.001)]
+		[TestCase(3600.0 * 1000.0, 1.0, 0, 0, 0.000001)] // 1h.
+		[TestCase(60.0 * 1000.0, 1.0/60, 1.0, 0, 0.0000001)] // 1m.
+		[TestCase(1.0 * 1000.0, 1.0 / 3600.0, 1/60.0, 1.0, 0.0000001)] // 1s.
+		[TestCase(50182113.33, 13.939475925925926, 56.36855555555555, 22.11333, 0.0000001)] // 13:56:22.11333
+		public void ConversionHMS(double milliseconds_of_day, double expected_hours, double expected_minutes, double expected_seconds, double epsilon)
+		{
+			var hms = new TimeHMS();
+			hms.UpdateTime(milliseconds_of_day);
+			Assert.IsTrue(hms.MillisecondsOfDay - epsilon <= milliseconds_of_day && milliseconds_of_day <= hms.MillisecondsOfDay + epsilon);
+			Assert.IsTrue(hms.CurrentHours - epsilon <= expected_hours && expected_hours <= hms.CurrentHours + epsilon);
+			Assert.IsTrue(hms.CurrentMinutes - epsilon <= expected_minutes && expected_minutes <= hms.CurrentMinutes + epsilon);
+			Assert.IsTrue(hms.CurrentSeconds - epsilon <= expected_seconds && expected_seconds <= hms.CurrentSeconds + epsilon);
+		}
+
+		/// <summary>
+		/// Test that updating a TimeHMS object
+		/// results in changed values from getters.
+		/// 
+		/// </summary>
+		/// <param name="time_pre">Time pre.</param>
+		/// <param name="time_post">Time post.</param>
+		[Test]
+		[TestCase(3400.0, 3232.0)]
+		[TestCase(3500.0, 0.0)]
+		public void UpdateCausesChange(double time_pre, double time_post)
+		{
+			var hms = new TimeHMS();
+
+			var epsilon = 0.001;
+			hms.UpdateTime(time_pre);
+			Assert.IsTrue(hms.MillisecondsOfDay - epsilon <= time_pre && time_pre <= hms.MillisecondsOfDay + epsilon);
+
+			var pre_hours = hms.CurrentHours;
+			var pre_minutes = hms.CurrentMinutes;
+			var pre_seconds = hms.CurrentSeconds;
+
+			hms.UpdateTime(time_post);
+			Assert.IsTrue(hms.MillisecondsOfDay - epsilon <= time_post && time_post <= hms.MillisecondsOfDay + epsilon);
+
+			Assert.AreNotEqual(hms.CurrentHours, pre_hours);
+			Assert.AreNotEqual(hms.CurrentMinutes, pre_minutes);
+			Assert.AreNotEqual(hms.CurrentSeconds, pre_seconds);
+		}
+
+		/// <summary>
+		/// Tests that we can round-trip convert from various times.
+		/// </summary>
+		[Test]
+		[TestCase(0.0, 0.001)]
+		[TestCase(0.001, 0.001)]
+		[TestCase(13.4 * 3600.0 * 1000.0, 0.001)]
+		public void ConversionRoundtrip(double milliseconds_of_day, double epsilon)
+		{
+			var hms = new TimeHMS();
+
+			hms.UpdateTime(milliseconds_of_day);
+			var reconverted = hms.MillisecondsOfDay;
+			Assert.IsTrue(milliseconds_of_day - epsilon <= reconverted && reconverted <= milliseconds_of_day + epsilon);
+
+		}
+
+	}
+
+
 	class ClockColors
 	{
 		/// <summary>
@@ -250,7 +406,7 @@ namespace gtksharp_clock
 
 				var thepoints = from coord in coords
 								select new Gdk.Point(coord[0] + 300, coord[1] + 300);
-				
+
 				this.GdkWindow.DrawPolygon(gc, true, thepoints.ToArray());
 			}
 		}
@@ -306,5 +462,4 @@ namespace gtksharp_clock
 			Assert.IsTrue(70.0 <= coords[1] && coords[1] <= 73.0, "For 7:30 y, actual coords are {0}, {1}", coords[0], coords[1]);
 		}
 	}
-
 }
